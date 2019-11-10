d19a60df7b62198d715338c1731a8e616df6f944
Cameron Flint <dev@caflint.fastmail.com>
2019-11-10 13:44:35 -0800

    Better tmux switching, inside and outside

--- sh/.funcs/tmux.sh
+++ sh/.funcs/tmux.sh
@@ -1,45 +1,19 @@
 #!/usr/bin/env sh
 
-# fzftmuxlist - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
-# `fzftmuxlist` will allow you to select your tmux session via fzf.
-# `fzftmuxlist` will attach to the irc session (if it exists), else it will create it.
-fzftmuxswitch() {
+# tmux-switch: quickly create or switch to a session, inside or outside of Tmux.
+tmux-switch() {
+  sid=$1
+  if [[ -z "$sid" ]]; then
+    local -r fmt='#{session_id}:|#S|(#{session_attached} attached)'
+    sid=$({ tmux display-message -p -F "$fmt" && tmux list-sessions -F "$fmt"; } \
+      | awk '!seen[$1]++' \
+      | column -t -s'|' \
+      | fzf -q '$' --reverse --prompt 'switch session: ' -1 \
+      | cut -d':' -f1)
+  fi
+  local change
   [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-  if [ $1 ]; then
-    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
-  fi
-  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0 --header="[[tmux-switch]]") &&  tmux $change -t "$session" || echo "No sessions found."
-}
-
-# zsh; needs setopt re_match_pcre. You can, of course, adapt it to your own shell easily.
-fzftmuxkill () {
-    local sessions
-    sessions="$(tmux ls|fzf --exit-0 --multi --header='[[tmux-kill]]')"  || return $?
-    local i
-    for i in "${(f@)sessions}"
-    do
-        [[ $i =~ '([^:]*):.*' ]] && {
-            echo "Killing $match[1]"
-            tmux kill-session -t "$match[1]"
-        }
-    done
-}
-
-function tmux-switch() {
-  if [[ $# != 1 ]]; then
-    # No arg: display picker.
-    name=$(tmux ls | fzf | cut -d: -f1)
-    if [[ -n $name ]]; then
-      tmux attach -t $name
-    fi
-  else
-    # One arg: switch to session, or create if not found.
-    if tmux has-session -t $1 &> /dev/null; then
-      tmux attach -t $1
-    else
-      tmux new-session -s $1 -n $1
-    fi
-  fi
+  tmux $change -t "$sid" 2>/dev/null || { tmux new-session -d -s $sid && tmux attach-session -t "$sid"; }
 }
 
 alias t='tmux-switch'
