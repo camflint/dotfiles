c2a82a378c37c520fc7c7a352eab20042fba2266
Cameron Flint <dev@caflint.fastmail.com>
2020-01-06 17:35:20 -0800

    More resilient tx-update-main-pane-dims script

--- tmux/.scripts/tx-update-main-pane-dims.sh
+++ tmux/.scripts/tx-update-main-pane-dims.sh
@@ -3,24 +3,30 @@ exit; 1; }
 
 while getopts 'w:h:' opt; do
   case "$opt" in
-    w) width=$OPTARG ;;
-    h) height=$OPTARG ;;
+    w) pwidth=$OPTARG ;;
+    h) pheight=$OPTARG ;;
   esac
 done
 
-if [[ $width -le 0 ]] || [[ $width -gt 100 ]] ||\
-   [[ $height -le 0 ]] || [[ $height -gt 100 ]]; then
+if [[ $pwidth -le 0 ]] || [[ $pwidth -gt 100 ]] ||\
+   [[ $pheight -le 0 ]] || [[ $pheight -gt 100 ]]; then
   echo 'ERROR: width and height must be percentage values between 1 and 100' >&2;
   exit 1
 fi
 
-cwidth=$(tmux display-message -pF '#{client_width}')
-cheight=$(tmux display-message -pF '#{client_height}')
+wwidth=$(tmux display-message -pF '#{window_width}')
+wheight=$(tmux display-message -pF '#{window_height}')
 
-pwidth=$(expr $cwidth \* $width \/ 100)
-pheight=$(expr $cheight \* $height \/ 100)
+width=$(echo "$wwidth * $pwidth / 100" | bc)
+# NOTE: it's important we leave 1 line of space for the status line, otherwise tmux will start to have layout issues and
+# crash a lot.
+height=$(echo "($wheight * $pheight / 100) - 2" | bc)
 
-tmux set-window-option main-pane-width $pwidth
-tmux set-window-option main-pane-height $pheight
-tmux select-layout
+# Set main-pane-width and main-pane-height
+[[ $width -gt 0 && $width -le $wwidth ]] && tmux set-window-option main-pane-width $width
+[[ $height -gt 0 && $height -le $wheight ]] && tmux set-window-option main-pane-height $height
+
+#echo "wheight=$wheight, height=$height"
+
+tmux select-layout  # refresh
 
